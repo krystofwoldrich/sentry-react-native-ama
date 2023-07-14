b1b7007e1722a323d663db9438d31467245e82dc
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-07-14 16:29:05 +0200

    Switch to new arch, add network calls, update deps

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+
+const BANNER_URL = 'https://33fa1ur95-qdasx2mqb.sentry.dev/static/8887640552ba90952651464f9df61ce5/88556/welcome-page-performance.webp';
+const DATA_URL = 'https://api.artic.edu/api/v1/artworks/129884';
 
 export default function App() {
+  const [data, setData] = React.useState(null);
+  const loadData = () => {
+    fetch(DATA_URL)
+    .then((response) => response.json())
+    .then((json) => setData(json.data.thumbnail.alt_text));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Image
+          style={styles.banner}
+          source={{
+          uri: BANNER_URL,
+        }}
+      />
+      <Text style={styles.data}>{data}</Text>
+      <Button
+        title='Load data'
+        onPress={loadData}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +36,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
   },
+  banner: {
+    width: '100%',
+    height: 200,
+    marginBottom: 20,
+ },
+ data: {
+  height: 100,
+  margin: 20,
+ }
 });
