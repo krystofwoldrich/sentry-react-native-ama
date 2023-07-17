b38156e769d8d650c210424db9e5be7721601b51
Krystof Woldrich <krystof.woldrich@sentry.io>
2023-07-17 12:07:37 +0200

    Change to nightly RN app

--- js/NativeTurboCrashModule.ts
+++
@@ -1,8 +0,0 @@
-import type {TurboModule} from 'react-native/Libraries/TurboModule/RCTExport';
-import {TurboModuleRegistry} from 'react-native';
-
-export interface Spec extends TurboModule {
-  getDataCrash(): string;
-}
-
-export default TurboModuleRegistry.get<Spec>('TurboCrashModule') as Spec | null;
